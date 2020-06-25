469e255775a03f2e8b01b04f5a57e814f0324223
Andrea <miranda.andrea.nd@gmail.com>
2020-06-25 16:57:18 +0200

    esercizio Fizz Buzz

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+for (var num = 1; num <= 100; num++) {
+  // console.log(num);
+
+  if (num % 3 == 0 && num % 5 == 0){
+    console.log("FizzBuzz");
+
+  }else if (num % 3 == 0){
+    console.log("Fizz");
+
+  }else if (num % 5 == 0){
+  console.log("Buzz");
+
+  }else {
+    console.log(num);
+  }
+}
+document.getElementById('id')
